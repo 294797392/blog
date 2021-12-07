4e1dd836908f273cb4e3f521c478a23cf673c6f8
294797392 <294797392@qq.com>
2021-12-07 18:06:49 +0800

    .

--- server/source/mm_main.c
+++ server/source/mm_main.c
@@ -1,18 +1,42 @@
-﻿#include <stdlib.h>
+﻿#include <libY/Yfirstinclude.h>
+
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#ifdef Y_API_WIN32
+#include <WinSock2.h>
+#elif Y_API_UNIX
+#endif
+
 // libY
 #include <libY/Ylog.h>
+#include <libY/Ythread.h>
+#include <libY/Ynet.h>
 
+// mm
 #include "mm_errno.h"
 #include "mm_config.h"
+
+static Ythread *svc_thread;
+static Ysocket ysock;
+
+static void svc_thread_entry(void *userdata)
+{
+    while(1)
+    {
+        // accept()
+    }
+}
 
 int main(int argc, char **argv)
 {
     Ylog_global_init();
 
     mmcfg_init();
+
+    ysock = Y_create_tcp_svc(NULL, mmcfg_get_port());
+    svc_thread = Y_create_thread(svc_thread_entry, NULL);
 
     YLOGI(YTEXT("mmsvc is running..."));
 
