abfe85af841266821bf8509abaec1ee834750144
294797392 <294797392@qq.com>
2022-09-29 08:35:25 +0800

    .

---
+++ server/external/libY/libYinc/Ybase.h
@@ -0,0 +1,90 @@
+﻿#ifndef __YBASE_H__
+#define __YBASE_H__
+
+#if (defined(Y_WIN32))
+#include <Windows.h>
+#elif (defined(Y_UNIX))
+#include <unistd.h>
+#endif
+
+// 定义导出和导入符号
+// 注意，如果是链接静态库，那么不需要__declspec(dllimport)
+#if (defined(Y_ENV_WIN32))
+    #ifdef Y_EXPORT
+        #define YAPI __declspec(dllexport)
+    #else
+        #define YAPI __declspec(dllimport)
+    #endif
+#elif (defined(Y_ENV_MINGW))
+    #ifdef Y_EXPORT
+        #define YAPI __declspec(dllexport)
+    #else
+        #define YAPI
+    #endif
+#else
+    #define YAPI
+#endif
+
+// 起个别名而已
+#define Y_API YAPI
+
+// 在unix和win32平台兼容多字节字符
+// YTEXT宏表示让编译器把YTEXT里的字符当成多字节字符处理，也就是一个字符可能需要2-4个字节去存储
+// 多字节字符可以存储汉字
+// #if (defined(Y_ENV_WIN32))
+// #define YTEXT(quote)		TEXT(quote)
+// #elif (defined(Y_ENV_MINGW))
+// #define YTEXT(quote)		L##quote
+// #elif (defined(Y_ENV_UNIX))
+// #define YTEXT(quote)        L##quote
+// #else
+// #define YTEXT(quote)        quote
+// #endif
+
+#define YTEXT
+typedef char YCHAR;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /*
+     * 描述：
+     * 睡眠指定的毫秒数
+     * 
+     * 参数：
+     * @mseconds：要睡眠的毫秒数
+     */
+    YAPI void Ysleep(int mseconds);
+
+#ifdef UNIX
+
+    YAPI int fd_read_until(int fd, char *buf, size_t len, char terminator);
+
+    /*
+     * 描述：
+     * 向一个文件描述符里写入数据，直到写完len大小或者失败才返回
+     * 
+     * 参数：
+     * @fd：要写入的文件描述符
+     * @data：要写入的数据地址
+     * @len：要写入的数据大小
+     * 
+     * 返回值：
+     * 写入成功返回0，写入失败返回1
+     */
+    YAPI int fd_write(int fd, void *data, size_t len);
+
+#endif
+
+    YAPI int getflag(int value, int flag);
+
+    YAPI int setflag(int value, int flag);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
---
+++ server/source/modules/service.c
@@ -0,0 +1,80 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <libY.h>
+
+#if (defined(Y_WIN32))
+#include <Windows.h>
+#elif (defined(Y_UNIX))
+#include <unistd.h>
+#endif
+
+#define DEFAULT_PORT		1018
+
+typedef struct service_module_s
+{
+    int fd;
+}service_module;
+
+int init_service_module(Ymodule *module, void **context)
+{
+#ifdef Y_UNIX
+	/*
+	 * 如果客户端socket已经关闭，服务端继续向socket写数据，内核会发出SIGPIPE信号。
+	 * 默认的操作是终止进程，但该信号可以被捕获并处理。
+	 * 设置SIG_IGN选项忽略该信号
+	 */
+	signal(SIGPIPE, SIG_IGN);
+#endif
+
+	int error = 0;
+	SOCKET s = -1;
+
+	// 新建一个socket
+	if((s = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+	{
+		error = zeus_os_error();
+		YLOGE(YTEXT("create socket failed, %d"), error);
+		return NULL;
+	}
+
+	// 设置Socket选项，当程序异常退出之后再次打开程序的时候端口还可以继续使用
+	int on = 1;
+	if(setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on)) < 0)
+	{
+		error = zeus_os_error();
+		YLOGE(YTEXT("setsockopt SO_REUSEADDR failed, %d"), error);
+		return NULL;
+	}
+
+	// 设置要侦听的网络接口和端口
+	int port = Y_module_config_get_int(module, "port", DEFAULT_PORT);
+	struct sockaddr_in bdaddr;
+	bdaddr.sin_family = PF_INET;
+	bdaddr.sin_port = htons(port);
+	bdaddr.sin_addr.s_addr = INADDR_ANY;
+	memset(&(bdaddr.sin_zero), 0, sizeof(bdaddr.sin_zero));
+
+	// 绑定
+	if(bind(s, (struct sockaddr *)&bdaddr, sizeof(struct sockaddr)) < 0)
+	{
+		error = zeus_os_error();
+		YLOGE(YTEXT("socket bind failed, %d"), error);
+		return NULL;
+	}
+
+	service_module *svc = (service_module*)Ycalloc(1, sizeof(service_module));
+	svc->fd = s;
+
+	*context = svc;
+
+	return YERR_SUCCESS;
+}
+
+void release_service_module(Ymodule *module)
+{
+
+}
+
+
